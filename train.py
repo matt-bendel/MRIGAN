5c2f3259eeea7ef85e15392f3b51ae8c46158d32
Matt Bendel <bendel.8@osu.edu>
2021-10-04 19:47:29 -0400

    fixed another typo

--- train.py
+++ train.py
@@ -159,7 +159,7 @@ def compute_gradient_penalty(D, real_samples, fake_samples, args):
 
 def generate_image(fig, target, image, title, image_ind):
     # rows and cols are both previously defined ints
-    ax = fig.add_subplot(2, 5, image_ind)
+    ax = fig.add_subplot(2, 6, image_ind)
     ax.set_title(title)
     ax.imshow(np.abs(image), cmap='gray', vmin=0, vmax=np.max(target))
     ax.set_xticks([])
@@ -169,7 +169,7 @@ def generate_image(fig, target, image, title, image_ind):
 def generate_error_map(fig, target, recon, image_ind, k=5):
     # Assume rows and cols are available globally
     # rows and cols are both previously defined ints
-    ax = fig.add_subplot(2, 5, image_ind)  # Add to subplot
+    ax = fig.add_subplot(2, 6, image_ind)  # Add to subplot
 
     # Normalize error between target and reconstruction
     error = np.abs(target - recon)
