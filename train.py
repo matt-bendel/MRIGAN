5d963b30ea52cf0140769b52984681f1c2258eb8
Matt Bendel <bendel.8@osu.edu>
2021-10-06 01:55:01 -0400

    another test

--- train.py
+++ train.py
@@ -171,7 +171,7 @@ def generate_image(fig, target, image, title, image_ind):
     ax.set_yticks([])
 
 
-def generate_error_map(fig, target, recon, image_ind, k=5):
+def generate_error_map(fig, target, recon, image_ind, k=5, max):
     # Assume rows and cols are available globally
     # rows and cols are both previously defined ints
     ax = fig.add_subplot(2, 3, image_ind)  # Add to subplot
@@ -179,7 +179,7 @@ def generate_error_map(fig, target, recon, image_ind, k=5):
     # Normalize error between target and reconstruction
     error = np.abs(target - recon)
     # normalized_error = error / error.max() if not relative else error
-    im = ax.imshow(k * error, cmap='jet')  # Plot image
+    im = ax.imshow(k * error, cmap='jet', vmax=max)  # Plot image
 
     # Remove axis ticks
     ax.set_xticks([])
@@ -223,8 +223,9 @@ def plot_epoch(args, generator, epoch):
     generate_image(fig, target_im, zfr, 'ZFR', 2)
     generate_image(fig, target_im, z_1_im, 'Z 1', 3)
 
-    generate_error_map(fig, target_im, zfr, 5, 7)
-    generate_error_map(fig, target_im, z_1_im, 6, 7)
+    max_val = np.max(np.abs(target_im - zfr))
+    generate_error_map(fig, target_im, zfr, 5, 7, max_val)
+    generate_error_map(fig, target_im, z_1_im, 6, 7, max_val)
 
     plt.savefig(
         f'/home/bendel.8/Git_Repos/MRIGAN/training_images/2_chan_z_mid/gen_{args.network_input}_{args.z_location}_{epoch + 1}.png')
