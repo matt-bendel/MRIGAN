8e25a66b6dccc361832073ff95ed3356602d1b8f
Matt Bendel <bendel.8@osu.edu>
2021-10-05 21:55:20 -0400

    fixed typo

--- train.py
+++ train.py
@@ -113,6 +113,7 @@ def prep_input_2_chan(data_tensor, unet_type, disc=False):
             output_tensor[:, :, :, 1] = output[8:16, :, :]
             output_x = ifft2c_new(output_tensor)
             output_x = transforms.root_sum_of_squares(output_x)
+            output_x = fft2c_new(output_x)
 
             disc_inp[k, :, :, :] = output_x.permute(2, 0, 1)
     else:
