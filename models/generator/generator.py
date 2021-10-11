9aaed9b7f22a8241aed850d102755cd26e286c2d
Matt Bendel <bendel.8@osu.edu>
2021-10-11 12:10:47 -0400

    fixed concat issue

--- models/generator/generator.py
+++ models/generator/generator.py
@@ -214,7 +214,6 @@ class GeneratorModel(nn.Module):
         if self.z_location == 2:
             z_out = self.middle_z_grow_linear(z)
             z_out = torch.reshape(z_out, (output.shape[0], self.latent_size // 4, 3, 3))
-            z_out = F.interpolate(z_out, scale_factor=2, mode='bilinear', align_corners=False)
             z_out = self.middle_z_grow_conv(z_out)
             output = torch.cat([output, z_out], dim=1)
             output = self.middle(output)
