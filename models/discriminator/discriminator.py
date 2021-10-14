dc57a9c2d7aa980fa6a09a779ed198cf7fc23e6c
Matt Bendel <bendel.8@osu.edu>
2021-10-13 20:41:58 -0400

    trying another disc arch

--- models/discriminator/discriminator.py
+++ models/discriminator/discriminator.py
@@ -110,8 +110,7 @@ class DiscriminatorModel(nn.Module):
 
         # CHANGE BACK TO 16 FOR MORE
         self.initial_layers = nn.Sequential(
-            nn.Conv2d(self.in_chans, 32, kernel_size=(3, 3), padding=1),  # 384x384
-            nn.InstanceNorm2d(64),
+            nn.Conv2d(self.in_chans, 64, kernel_size=(3, 3), padding=1),  # 384x384
             nn.LeakyReLU()
             # ResidualBlock(32, 32, False),
         )
@@ -119,9 +118,9 @@ class DiscriminatorModel(nn.Module):
         self.encoder_layers = nn.ModuleList()
         # self.encoder_layers += [FullDownBlock(16, 32)]  # 192x192
         # self.encoder_layers += [FullDownBlock(32, 64)]  # 96x96
-        self.encoder_layers += [FullDownBlock(32, 64)]  # 48x48
-        self.encoder_layers += [FullDownBlock(64, 128)]  # 24x24
-        self.encoder_layers += [FullDownBlock(128, 256)]  # 12x12
+        self.encoder_layers += [FullDownBlock(64, 128)]  # 48x48
+        self.encoder_layers += [FullDownBlock(128, 256)]  # 24x24
+        self.encoder_layers += [FullDownBlock(256, 512)]  # 12x12
         self.encoder_layers += [FullDownBlock(256, 512)]  # 6x6
         self.encoder_layers += [FullDownBlock(512, 512)]  # 3x3
         # self.encoder_layers += nn.Sequential(
