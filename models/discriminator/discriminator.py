6a0a971f0f30839a645e0bbe450c14b0395feeea
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2021-10-16 00:42:17 -0400

    another change

--- models/discriminator/discriminator.py
+++ models/discriminator/discriminator.py
@@ -110,19 +110,18 @@ class DiscriminatorModel(nn.Module):
 
         # CHANGE BACK TO 16 FOR MORE
         self.initial_layers = nn.Sequential(
-            nn.Conv2d(self.in_chans, 16, kernel_size=(3, 3), padding=1),  # 384x384
+            nn.Conv2d(self.in_chans, 32, kernel_size=(3, 3), padding=1),  # 384x384
             nn.LeakyReLU()
-            # ResidualBlock(32, 32, False),
         )
 
         self.encoder_layers = nn.ModuleList()
         # self.encoder_layers += [FullDownBlock(16, 32)]  # 192x192
         # self.encoder_layers += [FullDownBlock(32, 64)]  # 96x96
-        self.encoder_layers += [FullDownBlock(16, 32)]  # 48x48
-        self.encoder_layers += [FullDownBlock(32, 64)]  # 24x24
-        self.encoder_layers += [FullDownBlock(64, 128)]  # 12x12
-        self.encoder_layers += [FullDownBlock(128, 256)]  # 6x6
-        self.encoder_layers += [FullDownBlock(256, 512)]  # 3x3
+        self.encoder_layers += [FullDownBlock(32, 64)]  # 48x48
+        self.encoder_layers += [FullDownBlock(64, 128)]  # 24x24
+        self.encoder_layers += [FullDownBlock(128, 256)]  # 12x12
+        self.encoder_layers += [FullDownBlock(256, 512)]  # 6x6
+        self.encoder_layers += [FullDownBlock(512, 1024)]  # 3x3
         # INCLUDE BELOW FOR IMAGE
         # self.encoder_layers += nn.Sequential(
         #     nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1),
@@ -137,7 +136,7 @@ class DiscriminatorModel(nn.Module):
 
         self.dense = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(512 * 3 * 3, 1),
+            nn.Linear(1024 * 3 * 3, 1),
         )
 
     def forward(self, input):
