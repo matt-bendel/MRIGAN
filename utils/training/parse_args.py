949a1a02bcdc0da6768b4dfecf4a5eb47b64f63c
Matt Bendel <bendel.8@osu.edu>
2021-10-21 11:45:26 -0400

    removed print statements

--- utils/training/parse_args.py
+++ utils/training/parse_args.py
@@ -19,7 +19,7 @@ def create_arg_parser():
     parser.add_argument('--batch-size', default=16, type=int, help='Mini batch size')
     parser.add_argument('--num-epochs', type=int, default=300, help='Number of training epochs')
     parser.add_argument('--lr', type=float, default=10e-4, help='Learning rate')
-    parser.add_argument('--beta_1', type=float, default=0.5, help='Beta 1 for Adam')
+    parser.add_argument('--beta_1', type=float, default=0, help='Beta 1 for Adam')
     parser.add_argument('--beta_2', type=float, default=0.9, help='Beta 2 for Adam')
 
     # DATA ARGS
