a23be513d8cab5fefa286b14a8f985db21bdb427
Matt Bendel <bendel.8@osu.edu>
2021-10-22 13:32:41 -0400

    removed print statements

--- train.py
+++ train.py
@@ -281,7 +281,7 @@ def main(args):
                 # Loss measures generator's ability to fool the discriminator
                 # Train on fake images
                 fake_validity = discriminator(disc_inp)
-                g_loss = -0.1*torch.mean(fake_validity) + 10*F.l1_loss(disc_target_batch, disc_inp) - 10 * mssim_tensor(disc_target_batch, disc_inp)
+                g_loss = -0.1*torch.mean(fake_validity) + 10*F.l1_loss(disc_target_batch, disc_inp) -  * mssim_tensor(disc_target_batch, disc_inp)
 
                 g_loss.backward()
                 optimizer_G.step()
