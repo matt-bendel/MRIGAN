b821d3b426d3211fa36f9e0377e87257996ea3c3
Matt Bendel <bendel.8@osu.edu>
2021-10-27 23:20:42 -0400

    fixed issue

--- train_baseline_unet.py
+++ train_baseline_unet.py
@@ -103,7 +103,7 @@ def train_epoch(args, epoch, model, data_loader, optimizer, writer):
         target_im = prep_input_2_chan(target, args.network_input, args, disc=True).to(args.device).permute(0, 2, 3, 1)
         output_im = prep_input_2_chan(output, args.network_input, args, disc=True).to(args.device).permute(0, 2, 3, 1)
 
-        loss = mse(target_im, output_im) - mssim_tensor(target_im, output_im)
+        loss = 10 * mse(target_im, output_im) - 5 * mssim_tensor(target_im, output_im)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
