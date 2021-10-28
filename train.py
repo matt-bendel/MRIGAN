82fd535771b58d221260a0e6f7ad6941e77d8b53
Matt Bendel <bendel.8@osu.edu>
2021-10-28 18:09:06 -0400

    generator overhaul

--- train.py
+++ train.py
@@ -317,7 +317,9 @@ def main(args):
                 # Loss measures generator's ability to fool the discriminator
                 # Train on fake images
                 fake_validity = discriminator(disc_inp)
-                g_loss = -0.01 * torch.mean(fake_validity) + 15 * mse(disc_target_batch, disc_inp) - mssim_tensor(disc_target_batch, disc_inp)
+                adv = -0.01 * torch.mean(fake_validity).item()
+                print(adv)
+                g_loss = adv + 15 * mse(disc_target_batch, disc_inp) - mssim_tensor(disc_target_batch, disc_inp)
 
                 g_loss.backward()
                 optimizer_G.step()
