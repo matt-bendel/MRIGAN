5148d0ea63090271d335f2e0ba354f1b25e5a9ba
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2021-10-31 10:52:47 -0400

    changed test loading

--- train.py
+++ train.py
@@ -317,8 +317,7 @@ def main(args):
                 # Loss measures generator's ability to fool the discriminator
                 # Train on fake images
                 fake_validity = discriminator(disc_inp)
-                g_loss = -0.01 * torch.mean(fake_validity) - mssim_tensor(disc_target_batch, disc_inp) + 10 * mse(
-                    disc_target_batch, disc_inp)
+                g_loss = -0.01 * torch.mean(fake_validity) + 20 * F.l1_loss() - mssim_tensor(disc_target_batch, disc_inp)
 
                 g_loss.backward()
                 optimizer_G.step()
