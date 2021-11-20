d6a1422d3f7b757c392abcb8ab0dacdbcc52b1ab
Matt Bendel <bendel.8@osu.edu>
2021-11-20 13:59:12 -0500

    added new train script that penalizes lack of var on gen, mbsd on hold

--- train_3.py
+++ train_3.py
@@ -365,7 +365,7 @@ def main(args):
                     gen_pred_loss += torch.mean(fake_pred[k + 1])
 
                 var_loss = torch.mean(torch.var(disc_inputs_gen, (1, 2, 3, 4)))
-                var_weight = 0.0115
+                var_weight = 0.012
 
                 # TODO: BEST -0.001 adv and var_weight = 0.012
                 g_loss = -0.001*torch.mean(gen_pred_loss) + 0.001 * F.l1_loss(target_full, avg_recon) - mssim_tensor(
