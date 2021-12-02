f1ca97f1bd394c7d4389bc67ff50bd1aa521d1d1
Matt Bendel <bendel.8@osu.edu>
2021-12-02 01:17:30 -0500

    Debugging kspace plots and laying ground work for inpainting experiment

--- test_gen_variance.py
+++ test_gen_variance.py
@@ -104,8 +104,6 @@ def generate_image(fig, target, image, method, image_ind, rows, cols, kspace=Fal
             ax.set_title(f'PSNR: {psnr_val:.2f}, SNR: {snr_val:.2f}\nSSIM: {ssim_val:.4f}')
 
     if method == 'Std. Dev':
-        if kspace:
-            image = image**0.45
         im = ax.imshow(image, cmap='viridis')
         ax.set_xticks([])
         ax.set_yticks([])
