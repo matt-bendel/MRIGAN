647b2355b8d5735c24029323025e72e05a54de8a
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2021-12-21 16:16:18 -0500

    making changes for gan at scale

--- train_3.py
+++ train_3.py
@@ -117,7 +117,7 @@ def compute_gradient_penalty(D, real_samples, fake_samples, args, y):
     alpha = Tensor(np.random.random((real_samples.size(0), 1, 1, 1))).to(args.device)
     # Get random interpolation between real and fake samples
     interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True)
-    d_interpolates = D(interpolates, y)
+    d_interpolates = D(input=interpolates, y=y)
     fake = Tensor(real_samples.shape[0], 1).fill_(1.0).to(args.device)
     # Get gradient w.r.t. interpolates
     gradients = autograd.grad(
