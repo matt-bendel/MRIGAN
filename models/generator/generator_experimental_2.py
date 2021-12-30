2769edfb38923aa64c951af68dfdbec8245da6fd
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2021-12-30 15:34:52 -0500

    trying wider architecture, i.e. double channels of each layer

--- models/generator/generator_experimental_2.py
+++ models/generator/generator_experimental_2.py
@@ -121,7 +121,7 @@ class GeneratorModel(nn.Module):
 
         self.in_chans = in_chans
         self.out_chans = out_chans
-        self.chans = 16
+        self.chans = 32
         self.num_pool_layers = 7
         self.latent_size = latent_size
 
@@ -131,7 +131,7 @@ class GeneratorModel(nn.Module):
 
         self.down_sample_layers = nn.ModuleList([ConvDownBlock(in_chans, ch, batch_norm=False)])
         for i in range(num_pool_layers - 1):
-            if ch != 512:
+            if ch != 1024:
                 self.down_sample_layers += [ConvDownBlock(ch, ch * 2)]
                 ch *= 2
             else:
@@ -151,6 +151,8 @@ class GeneratorModel(nn.Module):
             nn.Conv2d(latent_size // 4, latent_size // 2, kernel_size=(3, 3), padding=1),
             nn.LeakyReLU(negative_slope=0.2),
             nn.Conv2d(latent_size // 2, latent_size, kernel_size=(3, 3), padding=1),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Conv2d(latent_size, latent_size * 2, kernel_size=(3, 3), padding=1),
             nn.LeakyReLU(negative_slope=0.2),
         )
         self.middle_z_grow_linear = nn.Sequential(
