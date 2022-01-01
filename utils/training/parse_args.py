246d5fdc87d06fde2f7d2bf9017afca88b30b8b4
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-01-01 13:46:25 -0500

    trying wider architecture, i.e. double channels of each layer

--- utils/training/parse_args.py
+++ utils/training/parse_args.py
@@ -24,8 +24,8 @@ def create_arg_parser():
     parser.add_argument('--num-epochs', type=int, default=100, help='Number of training epochs')
     # TODO: LR BACK TO 1e-4 AND BETA_2 BACK TO 0.9
     parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
-    parser.add_argument('--beta_1', type=float, default=0.9, help='Beta 1 for Adam')
-    parser.add_argument('--beta_2', type=float, default=0.999, help='Beta 2 for Adam')
+    parser.add_argument('--beta_1', type=float, default=0, help='Beta 1 for Adam')
+    parser.add_argument('--beta_2', type=float, default=0.99, help='Beta 2 for Adam')
 
     # DATA ARGS
     parser.add_argument('--data-parallel', required=True, action='store_true',
