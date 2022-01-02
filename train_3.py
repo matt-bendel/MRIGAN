d6b1e28635889eb03398b962d25bd70575c119d9
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-01-02 15:34:25 -0500

    trying wider architecture, i.e. double channels of each layer

--- train_3.py
+++ train_3.py
@@ -361,7 +361,7 @@ def main(args):
                 # var_loss = torch.mean(torch.var(disc_inputs_gen, dim=1) * torch.abs(target_full-avg_recon), dim=(0, 1, 2, 3))
                 var_loss = torch.mean(torch.var(disc_inputs_gen, dim=1), dim=(0, 1, 2, 3))
 
-                var_weight = 7.5e-3
+                var_weight = 0.005
                 adv_weight = 1e-6
                 mssim_weight = 0.84
 
