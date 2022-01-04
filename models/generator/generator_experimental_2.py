ce40db06d1f8602ae9628c1d9556db81d272e62e
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-01-04 16:47:23 -0500

    removed debug print comment

--- models/generator/generator_experimental_2.py
+++ models/generator/generator_experimental_2.py
@@ -122,7 +122,7 @@ class GeneratorModel(nn.Module):
         self.in_chans = in_chans
         self.out_chans = out_chans
         self.chans = 256
-        self.num_pool_layers = 5
+        self.num_pool_layers = 4
         self.latent_size = latent_size
 
         num_pool_layers = self.num_pool_layers
@@ -131,16 +131,15 @@ class GeneratorModel(nn.Module):
 
         self.down_sample_layers = nn.ModuleList([ConvDownBlock(in_chans, ch, batch_norm=False)])
         for i in range(num_pool_layers - 1):
-            if i != 3:
-                self.down_sample_layers += [ConvDownBlock(ch, ch * 2)]
-                ch *= 2
-            else:
-                self.down_sample_layers += [ConvDownBlock(ch, ch)]
+            self.down_sample_layers += [ConvDownBlock(ch, ch * 2)]
+            ch *= 2
 
         self.conv = nn.Sequential(
             nn.Conv2d(ch * 2, ch, kernel_size=3, padding=1),
             nn.BatchNorm2d(ch),
             nn.PReLU(),
+            ResidualBlock(ch),
+            ResidualBlock(ch),
             nn.Conv2d(ch, ch, kernel_size=3, padding=1),
             nn.BatchNorm2d(ch),
             nn.PReLU(),
@@ -168,11 +167,8 @@ class GeneratorModel(nn.Module):
 
         self.up_sample_layers = nn.ModuleList()
         for i in range(num_pool_layers - 1):
-            if i > 0:
-                self.up_sample_layers += [ConvUpBlock(ch * 2, ch // 2)]
-                ch //= 2
-            else:
-                self.up_sample_layers += [ConvUpBlock(ch * 2, ch)]
+            self.up_sample_layers += [ConvUpBlock(ch * 2, ch // 2)]
+            ch //= 2
 
         self.up_sample_layers += [ConvUpBlock(ch * 2, ch)]
         self.conv2 = nn.Sequential(
