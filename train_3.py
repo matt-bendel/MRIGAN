910dd0299b77588783a1c15d4835e4ba2466c735
Matt Bendel <bendel.8@osu.edu>
2022-01-07 18:37:44 -0500

    fixed measurement adding

--- train_3.py
+++ train_3.py
@@ -243,6 +243,7 @@ def gif_im(true, gen_im, index, type, disc_num=False):
     im, ax = generate_error_map(fig, true, gen_im, f'z {index}', 2, 2, 1)
 
     plt.savefig(f'/home/bendel.8/Git_Repos/full_scale_mrigan/MRIGAN/gifs/gif_{type}_{index - 1}.png')
+    plt.close()
 
 
 def generate_gif(type):
@@ -428,7 +429,7 @@ def main(args):
                 # var_loss = torch.mean(torch.var(disc_inputs_gen, dim=1) * torch.abs(target_full-avg_recon), dim=(0, 1, 2, 3))
                 var_loss = torch.mean(torch.var(disc_inputs_gen, dim=1), dim=(0, 1, 2, 3))
 
-                var_weight = 0.1
+                var_weight = 0.3
                 adv_weight = 1e-6
                 mssim_weight = 0.84
 
