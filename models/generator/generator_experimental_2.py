2c6060093c8b3618e73b6502a15149cd4a763db3
Matt Bendel <bendel.8@osu.edu>
2022-01-09 18:24:27 -0500

    More modification to gen arch

--- models/generator/generator_experimental_2.py
+++ models/generator/generator_experimental_2.py
@@ -200,7 +200,6 @@ class GeneratorModel(nn.Module):
         output = self.res_layer(output)
         output = torch.cat([z_out, output], dim=1)
         output = self.conv(output)
-        output = self.res_layer(output)
 
         # Apply up-sampling layers
         for layer in self.up_sample_layers:
