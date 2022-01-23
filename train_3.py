1e5ed4f4ae9e4556065247eaa93acdcc9566ead7
Matt Bendel <bendel.8@osu.edu>
2022-01-23 10:31:33 -0500

    changed gen architecture

--- train_3.py
+++ train_3.py
@@ -429,7 +429,7 @@ def main(args):
                 # var_loss = torch.mean(torch.var(disc_inputs_gen, dim=1) * torch.abs(target_full-avg_recon), dim=(0, 1, 2, 3))
                 var_loss = torch.mean(torch.var(disc_inputs_gen, dim=1), dim=(0, 1, 2, 3))
 
-                var_weight = 0.05
+                var_weight = 0.1
                 adv_weight = 1e-6
                 ssim_weight = 0.84
 
