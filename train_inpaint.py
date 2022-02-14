06d66bf18b3a99940a90c958616e63ec1ef5ae76
Matt Bendel <bendel.8@osu.edu>
2022-02-14 18:18:27 -0500

    debugging inpaint

--- train_inpaint.py
+++ train_inpaint.py
@@ -267,7 +267,7 @@ def main(args):
 
     args.in_chans = 1
     args.out_chans = 1
-    args.batch_size = 32
+    # args.batch_size = 32
 
     if args.resume:
         generator, optimizer_G, discriminator, optimizer_D, args, best_loss_val, best_loss_dis, start_epoch = get_gen_supervised(
@@ -367,9 +367,9 @@ def main(args):
             var_weight = 0.02
             adv_weight = 1e-6
             ssim_weight = 0.84
-            g_loss = -adv_weight * torch.mean(gen_pred_loss)
-            g_loss += (1 - ssim_weight) * F.l1_loss(target, avg_recon) - ssim_weight * mssim_tensor(target, avg_recon)
-            g_loss += - var_weight * torch.mean(torch.var(disc_inputs_gen, dim=1), dim=(0, 1, 2, 3))
+            # g_loss = -adv_weight * torch.mean(gen_pred_loss)
+            # g_loss += (1 - ssim_weight) * F.l1_loss(target, avg_recon) - ssim_weight * mssim_tensor(target, avg_recon)
+            g_loss = - var_weight * torch.mean(torch.var(disc_inputs_gen, dim=1), dim=(0, 1, 2, 3))
 
             g_loss.backward()
             optimizer_G.step()
