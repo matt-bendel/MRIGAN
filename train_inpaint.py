8b6478abec32bc436f2718fb06c86d12498f5378
Matt Bendel <bendel.8@osu.edu>
2022-02-14 18:22:53 -0500

    debugging inpaint

--- train_inpaint.py
+++ train_inpaint.py
@@ -367,8 +367,8 @@ def main(args):
             var_weight = 0.02
             adv_weight = 1e-6
             ssim_weight = 0.84
-            g_loss = -adv_weight * torch.mean(gen_pred_loss)
-            # g_loss += (1 - ssim_weight) * F.l1_loss(target, avg_recon) - ssim_weight * mssim_tensor(target, avg_recon)
+            # g_loss = -adv_weight * torch.mean(gen_pred_loss)
+            g_loss = (1 - ssim_weight) * F.l1_loss(target, avg_recon) - ssim_weight * mssim_tensor(target, avg_recon)
             # g_loss += - var_weight * torch.mean(torch.var(disc_inputs_gen, dim=1), dim=(0, 1, 2, 3))
 
             g_loss.backward()
