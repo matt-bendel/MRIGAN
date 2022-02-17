90f24337314a3ae02199f216371e21ad98dad120
Matt Bendel <bendel.8@osu.edu>
2022-02-17 10:57:08 -0500

    Changed ablation

--- train_ablation.py
+++ train_ablation.py
@@ -430,9 +430,9 @@ def main(args):
                 gen_pred_loss += torch.mean(fake_pred[k + 1])
 
             var_weight = 0.02
-            adv_weight = 1e-6
+            adv_weight = 1e-6 if args.supervised else 1e-3
             ssim_weight = 0.84
-            g_loss = -1e-3*torch.mean(gen_pred_loss) if args.adv_only else -adv_weight * torch.mean(gen_pred_loss)
+            g_loss = -adv_weight*torch.mean(gen_pred_loss) if args.adv_only else 0
             g_loss += (1 - ssim_weight) * F.l1_loss(target_full, avg_recon) - ssim_weight * mssim_tensor(target_full,
                                                                                                          avg_recon) if args.supervised else 0
             g_loss += - var_weight * torch.mean(torch.var(disc_inputs_gen, dim=1),
