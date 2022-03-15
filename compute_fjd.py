f869f424cbcab0f6a05d7c46d7a0eeaa85ce3f64
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-03-15 19:38:31 -0400

    Fixed matrix multiply

--- compute_fjd.py
+++ compute_fjd.py
@@ -176,13 +176,47 @@ cuda - If True, indicates that the GPU accelerated version of FJD should be
 
 
 def main(args):
+    patch_study = False
     print("GETTING DATA LOADERS")
-    ref_loader, cond_loader = get_dataloaders(args)
     print("GETTING INCEPTION EMBEDDING")
     inception_embedding = InceptionEmbedding(parallel=True)
     print("GETTING GENERATOR")
     max = 6 if not args.inpaint and not args.adler else 1
-    cfid = True
+
+    if patch_study:
+        args.patches = True
+        for i in range(3):
+            args.num_patches = 2**(i+1)
+            ref_loader, cond_loader = get_dataloaders(args)
+            num_samps = 32
+            args.z_location = 1
+            gan = get_gen(args)
+            gan = GANWrapper(gan, args)
+            print("COMPUTING METRIC")
+            fjd_metric = FJDMetric(gan=gan,
+                                   reference_loader=ref_loader,
+                                   condition_loader=cond_loader,
+                                   image_embedding=inception_embedding,
+                                   condition_embedding=inception_embedding,
+                                   save_reference_stats=False,
+                                   samples_per_condition=num_samps,
+                                   cuda=True,
+                                   args=args)
+
+            print(f"FID FOR NETWORK {args.z_location}")
+            fid = fjd_metric.get_fid()
+            fjd = fjd_metric.get_fjd(alpha=1.097)
+            print('FID: ', fid)
+            print('FJD: ', fjd)
+            cfid_val = fjd_metric.get_cfid(fjd_metric.true_im_embeds, fjd_metric.true_im_cond_embeds)
+            print('CFID: ', cfid_val)
+            del gan
+            del fjd_metric
+            del ref_loader
+            del cond_loader
+        exit()
+    else:
+        ref_loader, cond_loader = get_dataloaders(args)
 
     if args.noise_v_fjd:
         for i in range(6):
@@ -221,6 +255,8 @@ def main(args):
             del fjd_metric
 
     else:
+        true_im_embed = None
+        true_im_cond_embed = None
         for i in range(8):
             num_samps = 32
             args.z_location = i+1
@@ -250,14 +286,17 @@ def main(args):
                     any conditional consistency.
                     '''
             print(f"FID FOR NETWORK {args.z_location}")
-            if not cfid:
-                fid = fjd_metric.get_fid()
-                fjd = fjd_metric.get_fjd(alpha=1.097)
-                print('FID: ', fid)
-                print('FJD: ', fjd)
-            else:
-                cfid_val = fjd_metric.get_cfid()
-                print('CFID: ', cfid_val)
+            fid = fjd_metric.get_fid()
+            if i == 0:
+                true_im_embed = fjd_metric.true_im_embeds
+                true_im_cond_embed = fjd_metric.true_im_cond_embeds
+
+            fjd = fjd_metric.get_fjd(alpha=1.097)
+            print('FID: ', fid)
+            print('FJD: ', fjd)
+            cfid_val = fjd_metric.get_cfid(true_im_embed, true_im_cond_embed)
+            print('CFID: ', cfid_val)
+            exit()
             del gan
             del fjd_metric
 
--- fjd_metric.py
+++ fjd_metric.py
@@ -55,6 +55,10 @@ class FJDMetric:
         self.samples_per_condition = samples_per_condition
         self.cuda = cuda
         self.eps = eps
+        self.fake_im_embeds = None
+        self.fake_im_cond_embeds = None
+        self.true_im_embeds = None
+        self.true_im_cond_embeds = None
 
         self.mu_fake, self.sigma_fake = None, None
         self.mu_real, self.sigma_real = None, None
@@ -126,8 +130,8 @@ class FJDMetric:
             image_embed = np.concatenate(image_embed, axis=0)
             cond_embed = np.concatenate(cond_embed, axis=0)
 
-        if cfid:
-            return image_embed, cond_embed
+        self.fake_im_embeds = image_embed
+        self.fake_im_cond_embeds = cond_embed
 
         mu_fake, sigma_fake = self._get_joint_statistics(image_embed, cond_embed)
         del image_embed
@@ -197,8 +201,8 @@ class FJDMetric:
             image_embed = np.concatenate(image_embed, axis=0)
             cond_embed = np.concatenate(cond_embed, axis=0)
 
-        if cfid:
-            return image_embed, cond_embed
+        self.true_im_embeds = image_embed
+        self.true_im_cond_embeds = cond_embed
 
         self._calculate_alpha(image_embed, cond_embed)
         mu_real, sigma_real = self._get_joint_statistics(image_embed, cond_embed)
@@ -254,10 +258,9 @@ class FJDMetric:
 
         return mu1, sigma1, mu2, sigma2
 
-    def get_cfid(self, resample=True):
-        x_true, y_1 = self._compute_reference_distribution(cfid=True)
+    def get_cfid(self, x_true, y_1):
         N1 = x_true.shape[0]
-        x_pred, y_2 = self._get_generated_distribution(cfid=True)
+        x_pred, y_2 = self.fake_im_embeds, self.fake_im_cond_embeds
         N2 = x_pred.shape[0]
 
         mu_x_true = torch.mean(x_true, 0)
@@ -276,8 +279,8 @@ class FJDMetric:
         sigma_x_pred_y = torch.mm(zero_mu_x_pred.t(), zero_mu_y2) / N2
         sigma_y_x_true = torch.mm(zero_mu_y1.t(), zero_mu_x_true) / N1
         sigma_y_x_pred = torch.mm(zero_mu_y2.t(), zero_mu_x_pred) / N2
-        sigma_y1_y1_inv = torch.inv(torch.mm(zero_mu_y1.t(), zero_mu_y1) / N1)
-        sigma_y2_y2_inv = torch.inv(torch.mm(zero_mu_y2.t(), zero_mu_y2) / N2)
+        sigma_y1_y1_inv = torch.inverse(torch.mm(zero_mu_y1.t(), zero_mu_y1) / N1)
+        sigma_y2_y2_inv = torch.inverse(torch.mm(zero_mu_y2.t(), zero_mu_y2) / N2)
 
         mu_x_true_given_y = mu_x_true + torch.mm(torch.mm(sigma_x_true_y, sigma_y1_y1_inv), y_1 - mu_y1)
         mu_x_pred_given_y = mu_x_pred + torch.mm(torch.mm(sigma_x_pred_y, sigma_y2_y2_inv), y_2 - mu_y2)
