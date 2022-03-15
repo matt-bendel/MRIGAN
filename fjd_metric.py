fc0d30248f9a9552055fc86a7009fa3cbbeebc87
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-03-15 18:59:09 -0400

    Fixed matrix multiply

--- fjd_metric.py
+++ fjd_metric.py
@@ -258,14 +258,14 @@ class FJDMetric:
         zero_mu_y1 = y_1 - mu_y1
         zero_mu_y2 = y_2 - mu_y2
 
-        sigma_x_true_x_true = torch.mm(torch.transpose(zero_mu_x_true, 0, 1), zero_mu_x_true) / N1
-        sigma_x_pred_x_pred = torch.mm(torch.transpose(zero_mu_x_pred, 0, 1), zero_mu_x_pred) / N2
-        sigma_x_true_y = torch.mm(torch.transpose(zero_mu_x_true, 0, 1), zero_mu_y1) / N1
-        sigma_x_pred_y = torch.mm(torch.transpose(zero_mu_x_pred, 0, 1), zero_mu_y2) / N2
-        sigma_y_x_true = torch.mm(torch.transpose(zero_mu_y1, 0, 1), zero_mu_x_true) / N1
-        sigma_y_x_pred = torch.mm(torch.transpose(zero_mu_y2, 0, 1), zero_mu_x_pred) / N2
-        sigma_y1_y1_inv = torch.inv(torch.mm(torch.transpose(zero_mu_y1, 0, 1), zero_mu_y1) / N1)
-        sigma_y2_y2_inv = torch.inv(torch.mm(torch.transpose(zero_mu_y2, 0, 1), zero_mu_y2) / N2)
+        sigma_x_true_x_true = torch.mm(zero_mu_x_true.t(), zero_mu_x_true) / N1
+        sigma_x_pred_x_pred = torch.mm(zero_mu_x_pred.t(), zero_mu_x_pred) / N2
+        sigma_x_true_y = torch.mm(zero_mu_x_true.t(), zero_mu_y1) / N1
+        sigma_x_pred_y = torch.mm(zero_mu_x_pred.t(), zero_mu_y2) / N2
+        sigma_y_x_true = torch.mm(zero_mu_y1.t(), zero_mu_x_true) / N1
+        sigma_y_x_pred = torch.mm(zero_mu_y2.t(), zero_mu_x_pred) / N2
+        sigma_y1_y1_inv = torch.inv(torch.mm(zero_mu_y1.t(), zero_mu_y1) / N1)
+        sigma_y2_y2_inv = torch.inv(torch.mm(zero_mu_y2.t(), zero_mu_y2) / N2)
 
         mu_x_true_given_y = mu_x_true + torch.mm(torch.mm(sigma_x_true_y, sigma_y1_y1_inv), y_1 - mu_y1)
         mu_x_pred_given_y = mu_x_pred + torch.mm(torch.mm(sigma_x_pred_y, sigma_y2_y2_inv), y_2 - mu_y2)
@@ -275,7 +275,7 @@ class FJDMetric:
         return calculate_fd(mu_x_true_given_y, sigma_x_true_x_true_given_y, mu_x_pred_given_y, sigma_x_pred_x_pred_given_y, cuda=self.cuda, eps=self.eps)
 
 
-    def get_fjd(self, alpha=None, resample=True):
+    def get_fjd(self, alpha=None, resample=False):
         """Calculate FJD.
 
         Args:
@@ -306,7 +306,7 @@ class FJDMetric:
         fjd = calculate_fd(m1, s1, m2, s2, cuda=self.cuda, eps=self.eps)
         return fjd
 
-    def get_fid(self, resample=True):
+    def get_fid(self, resample=False):
         """Calculate FID (equivalent to FJD at alpha = 0).
 
         Args:
