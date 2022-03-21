bcb501a7a59687627b10f2be3b492dd7e5299f29
Matt Bendel <bendel.8@osu.edu>
2022-03-20 23:41:48 -0400

    Yikes

--- fjd_metric.py
+++ fjd_metric.py
@@ -607,9 +607,9 @@ def sqrt_newton_schulz(A, numIters, dtype=None):
         batchSize = A.shape[0]
         dim = A.shape[1]
         normA = A.mul(A).sum(dim=1).sum(dim=1).sqrt()
-        Y = A.div(normA.view(batchSize, 1, 1).expand_as(A)).to('cuda:3')
-        I = torch.eye(dim, dim).view(1, dim, dim).repeat(batchSize, 1, 1).type(dtype).to('cuda:3')
-        Z = torch.eye(dim, dim).view(1, dim, dim).repeat(batchSize, 1, 1).type(dtype).to('cuda:3')
+        Y = A.div(normA.view(batchSize, 1, 1).expand_as(A)).to('cuda:1')
+        I = torch.eye(dim, dim).view(1, dim, dim).repeat(batchSize, 1, 1).type(dtype).to('cuda:1')
+        Z = torch.eye(dim, dim).view(1, dim, dim).repeat(batchSize, 1, 1).type(dtype).to('cuda:1')
         for i in range(numIters):
             T = 0.5 * (3.0 * I - Z.bmm(Y))
             Y = Y.bmm(T)
