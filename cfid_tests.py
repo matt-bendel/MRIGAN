a2372ce736885894500338926d8842f5c22311d1
Matt Bendel <bendel.8@osu.edu>
2022-03-24 17:36:08 -0400

    Testing CFID issues

--- cfid_tests.py
+++ cfid_tests.py
@@ -188,6 +188,7 @@ def get_cfid_torch_svd(y_predict, x_true, y_true, np_inv=False, mat=False):
 
     u, s, vh = torch.linalg.svd(no_m_x_true, full_matrices=False)
     v = vh.t()
+    print(v.shape)
     c_dist_1 = torch.norm(torch.matmul(no_m_y_true.t() - no_m_y_pred.t(), v)) ** 2 / y_true.shape[0]
 
     v_t_v = torch.matmul(vh, v)
