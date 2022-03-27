7e218ff7f62e1c985bc821850d82257375f7208f
Matt Bendel <bendel.8@osu.edu>
2022-03-27 15:13:30 -0400

    Updated to plot cfid values

--- utils/training/prepare_data_fid.py
+++ utils/training/prepare_data_fid.py
@@ -195,7 +195,7 @@ def create_datasets(args, val_only):
         use_top_slices=True,
         number_of_top_slices=args.num_of_top_slices,
         restrict_size=False,
-        big_test=True
+        big_test=False
     )
 
     return dev_data, train_data
