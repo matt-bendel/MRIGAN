951d75b169a35e735a4f6662bab9a4c923af94d4
Matt Bendel <bendel.8@osu.edu>
2022-03-28 11:11:33 -0400

    Updated to plot cfid values

--- cfid_tests.py
+++ cfid_tests.py
@@ -348,7 +348,7 @@ if __name__ == '__main__':
     labels = ['Full', '-Adversarial', '-Supervised', '-Variance Reward', '-DC', '-DI', 'Full (2)', 'Adler']
     x_axis = [1, 4, 16]
     if True:
-        out_dir = f'/storage/fastMRI_brain_T2_embeddings/1_sample/4_patch/'
+        out_dir = f'/storage/fastMRI_brain_T2_embeddings/1_sample/1_patch/'
 
         recon_embeds = []
         cond_embeds = []
