334d55aafddc8a4645023eb5c4e8b55c14964b57
Matt Bendel <bendel.8@osu.edu>
2022-03-27 22:15:26 -0400

    Updated to plot cfid values

--- fjd_metric_langevin.py
+++ fjd_metric_langevin.py
@@ -253,7 +253,7 @@ class FJDMetric:
         for fname in tqdm(os.listdir(ref_directory), desc='Computing Generated Distribution'):
             with torch.no_grad():
                 for i in range(6):
-                    for j in range(1):
+                    for j in range(32):
                         new_filename = recon_directory + fname + f'|langevin|slide_idx_{i}_R={R}_sample={j}_outputs.pt'
                         recon_object = torch.load(new_filename)
 
