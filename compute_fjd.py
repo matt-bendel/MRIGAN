a7e4ebe40b4c97b3159244947259c4059dde7f1b
Matt Bendel <bendel.8@osu.edu>
2022-04-02 01:02:36 -0400

    Updated to plot cfid values

--- compute_fjd.py
+++ compute_fjd.py
@@ -234,8 +234,8 @@ def main(args):
         else:
             args.patches = True
         print("PATCHES ", args.num_patches)
-        ref_loader, cond_loader = get_dataloaders(args)
         for noise_var in noise_vars:
+            ref_loader, cond_loader = get_dataloaders(args)
             for j in range(1):
                 # if j == 0 or j == 5 or j == 6:
                 #     continue
