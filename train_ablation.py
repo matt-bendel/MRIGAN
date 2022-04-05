3ce25d16ab6acff4e3ede3256f24cc6336d2b5e9
Matt Bendel <bendel.8@osu.edu>
2022-04-05 18:12:08 -0400

    Updated generator to use code vector placement 2

--- train_ablation.py
+++ train_ablation.py
@@ -426,7 +426,7 @@ def main(args):
                 gen_pred_loss += torch.mean(fake_pred[k + 1])
 
             var_weight = np.sqrt(2/(np.pi*args.num_z*(args.num_z+1)))
-            adv_weight = 1e-3
+            adv_weight = 1e-5
             g_loss = -adv_weight*torch.mean(gen_pred_loss)
             # g_loss += F.mse_loss(avg_recon, target_full) if args.supervised else 0
             g_loss += F.l1_loss(target_full, avg_recon) if args.supervised else 0
